7c54a80ab6305e3b09c5c43e8fbe7ec8fb5a2172
bingoo <bingoo.huang@gmail.com>
2017-11-11 09:54:00 +0800

    deploy bash

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# ./deploy.sh ci@ci01
+targetHost=$1
+deployName=go-ip-allow
+./gobin.sh
+rm -fr $deployName.linux.bin $deployName.linux.bin.bz2
+env GOOS=linux GOARCH=amd64 go build -o $deployName.linux.bin
+bzip2 $deployName.linux.bin
+rsync -avz --human-readable --progress -e "ssh -p 22" ./$deployName.linux.bin.bz2 $targetHost:./firewall
+ssh -tt $targetHost "bash -s" << eeooff
+cd ./firewall
+ps -ef|grep $deployName|grep -v grep|awk '{print \$2}'|xargs -r kill -9
+rm $deployName.linux.bin
+bzip2 -d $deployName.linux.bin.bz2
+nohup ./$deployName.linux.bin > $deployName.out 2>&1 &
+exit
+eeooff
